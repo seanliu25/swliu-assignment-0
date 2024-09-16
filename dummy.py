44d02bda974d2c359971d4a8098c8b1394fe1e3e
Shangwei Liu <shangweiliu@Macintosh.local>
2024-09-15 20:35:15 -0400

    first commit

---
+++ dummy.py
@@ -0,0 +1,8 @@
+def sum(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+     num1 = int(input("Enter the first number: "))
+     num2 = int(input("Enter the second number: "))
+     print("The sum is:", sum(num1, num2))
